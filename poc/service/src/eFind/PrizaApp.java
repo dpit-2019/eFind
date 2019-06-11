414d787086edf26fdd564825dab60697215b69f3
Jeff0220 <50880579+Jeff0220@users.noreply.github.com>
2019-06-11 18:00:01 +0300

    Serviciu (aproape) gata
    nu

---
+++ poc/service/src/eFind/PrizaApp.java
@@ -0,0 +1,11 @@
+package eFind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrizaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PrizaApp.class,args);
+    }
+}
