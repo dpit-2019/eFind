9ca3b37eec1cec9e2ee504050facc064ee9d5764
ARTSOFT\Alex Muresan <alex.muresan@artsoft-consult.ro>
2019-07-04 14:26:28 +0300

    Rezolvare problema deploy

--- src/main/java/ro/dpit/efind/App.java
+++ src/main/java/ro/dpit/efind/App.java
@@ -1,13 +1,40 @@
 package ro.dpit.efind;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello ");
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class App extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(applicationClass, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<App> applicationClass = App.class;
+}
+
+@RestController
+class GreetingController {
+
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
     }
 }
