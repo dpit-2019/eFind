f7a5fcf1c7f2e911e0162374abcca2f737b0f6f3
cosmincandrea <cosmin_candrea@yahoo.ro>
2019-07-15 23:03:19 +0300

    mai multe servicii

--- src/main/java/ro/dpit/efind/service/PrizaController.java
+++ src/main/java/ro/dpit/efind/service/PrizaController.java
@@ -4,10 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 
 @RestController
@@ -99,12 +96,83 @@ public class PrizaController {
             c.setAutoCommit(false);
             System.out.println("Opened database successfully");
             stmt = c.createStatement();
-             stmt.executeUpdate("INSERT INTO \"prizaEfind\".\"detalii\" VALUES (5,"+Nume+","+ tip+","+ Descriere+","+Latitude+","+Longitude+");");
+             stmt.executeUpdate("INSERT INTO \"prizaEfind\".\"detalii\" VALUES (7,"+Nume+","+ tip+","+ Descriere+","+Latitude+","+Longitude+");");
             stmt.close();
             c.commit();
             c.close();
         }catch(Exception e) {
             System.out.println("nu merge====================Adaugarea");
+            e.printStackTrace();
+        }
+    }
+    @RequestMapping("/getBackOfficeData")
+    public Prize getBackOfficeData()
+    {
+        int [] pending = new int[150];
+         int[] id = new int[150] ;
+         String[] Nume = new String[150] ;
+         int[] tip = new int[150]  ;
+         String[] Descriere =new String[150];
+         double[] lat = new double[150];
+         double[] lng = new double[150];
+        Prize rezulltat =  new Prize();
+        Connection c = null;
+        Statement stmt = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/EfindTestDB",
+                            "postgres", "password");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+            stmt = c.createStatement();
+            ResultSet rs= stmt.executeQuery("select * FROM \"prizaEfind\".\"detalii\"");
+            int i=1;
+            while(rs.next()) {
+                id[i] = rs.getInt("id");
+                Nume[i] = rs.getString("nume");
+                tip[i] = rs.getInt("tip");
+                Descriere[i] = rs.getString("descriere");
+                lat[i] = rs.getDouble("lat");
+                lng[i] = rs.getDouble("lng");
+                pending[i]=rs.getInt("pending");
+                i++;
+            }
+            stmt.close();
+            c.close();
+            rezulltat.setId(id);
+            rezulltat.setNume(Nume);
+            rezulltat.setTip(tip);
+            rezulltat.setDescriere(Descriere);
+            rezulltat.setLat(lat);
+            rezulltat.setLng(lng);
+            rezulltat.setPending(pending);
+        }catch(Exception e) {
+            System.out.println("=================BackOffice=============");
+            e.printStackTrace();
+        }
+        return rezulltat;
+
+    }
+    @RequestMapping("/changeStatus")
+    public void changeStatus (@RequestParam(value = "id") int id)
+    {
+        Connection c = null;
+        Statement stmt = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/EfindTestDB",
+                            "postgres", "password");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+            stmt = c.createStatement();
+            stmt.executeUpdate("UPDATE \"prizaEfind\".\"detalii\" set pending = 0 WHERE id="+id+";");
+            stmt.close();
+            c.commit();
+            c.close();
+        }catch(Exception e) {
+            System.out.println("*=======*");
             e.printStackTrace();
         }
     }
