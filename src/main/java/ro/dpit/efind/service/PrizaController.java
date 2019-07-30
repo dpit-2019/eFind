8dff2fa017eece78575e69fa75b5c3c3e661d933
cosmincandrea <cosmin_candrea@yahoo.ro>
2019-07-30 15:51:15 +0300

    ahitectura noua

--- src/main/java/ro/dpit/efind/service/PrizaController.java
+++ src/main/java/ro/dpit/efind/service/PrizaController.java
@@ -12,91 +12,20 @@ import java.util.*;
 @RestController
 public class PrizaController  {
 
-
-    ConnectionPool connectionPool;
-    {
-        try {
-            Class.forName("org.postgresql.Driver");
-            connectionPool = BasicConnectionPool
-                    .create("jdbc:postgresql://localhost:5432/efind", "postgres", "password");
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                public final void run() {
-                    try {
-                        connectionPool.shutdown();
-                        System.out.println("Merge shutdown hook");
-                    } catch (SQLException e) {
-                        System.out.println("NU merge shutdown");
-                        e.printStackTrace();
-                    }
-                }
-            });
-        } catch (SQLException| ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+    DBinterface DB = BasicDB.create();
     @RequestMapping("/getPriza")
-    public Priza getDetails(@RequestParam(value="id") Long id) {
-        Priza res = new Priza();
-        try {
-            Statement stmt = null;
-            Connection conn = connectionPool.getConnection();
-            conn.setAutoCommit(false);
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM \"priza\".\"detalii\" WHERE id=" + id + ";");
-            while(rs.next()) {
-                res.setId(id);
-                res.setName(rs.getString("nume"));
-                res.setTip(rs.getInt("tip"));
-                res.setDescriere(rs.getString("descriere"));
-                res.setLat(rs.getDouble("lat"));
-                res.setLng(rs.getDouble("lng"));
-                res.setPending(rs.getInt("status"));
-                res.setReports(rs.getInt("reports"));
-            }
-            System.out.println("servicul e accesat");
-            rs.close();
-            connectionPool.releaseConnection(conn);
-            stmt.close();
-        } catch (Exception e) {
-            System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-            e.printStackTrace();
+    public Priza getDetails(@RequestParam(value="id") int id) {
 
-        }
-        return res;
+        return DB.aduPriza(id);
+
     }
-
-
 
     @RequestMapping("/getPointeri")
     public List<MapPoint> getPrize(@RequestParam(value="lats") double lats,
                                    @RequestParam(value="lngs") double lngs,
                                    @RequestParam(value="latj") double latj,
                                    @RequestParam(value="lngj") double lngj ){
-        List<MapPoint> rez = new ArrayList<MapPoint>();
-        try{
-            Connection conn = connectionPool.getConnection();
-            Statement stmt = null;
-            conn.setAutoCommit(false);
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT lat, lng, id FROM \"priza\".\"detalii\" WHERE lat<"+lats+"AND lat>" +latj+
-                    "AND lng<"+lngs+"AND lng>"+lngj+"AND status=2;");
-            int t=0;
-            while(rs.next())
-            {
-                MapPoint to = new MapPoint();
-                to.setId(rs.getInt("id"));
-                to.setLng(rs.getDouble("lng"));
-                to.setLat(rs.getDouble("lat"));
-                rez.add(to);
-                t++;
-            }
-            System.out.println("merge getPointeri");
-            connectionPool.releaseConnection(conn);
-        }catch(Exception e) {
-            System.out.println("nu merge==========================================getPointeri");
-            e.printStackTrace();
-        }
-        return rez;
+       return DB.aduPointeri(latj, lngj, lats, lngs);
     }
 
 
@@ -107,120 +36,31 @@ public class PrizaController  {
                          @RequestParam(value = "descriere") String Descriere,
                          @RequestParam(value = "lat") double Latitude,
                          @RequestParam(value = "lng") double Longitude){
-
-        try{
-            Connection conn = connectionPool.getConnection();
-            Statement stmt = null;
-            conn.setAutoCommit(false);
-            stmt = conn.createStatement();
-            stmt.executeUpdate("INSERT INTO \"priza\".\"detalii\"(nume,tip, descriere, lat, lng ) VALUES ("+Nume+","+ tip+","+ Descriere+","+Latitude+","+Longitude+");");
-            stmt.close();
-            conn.commit();
-            connectionPool.releaseConnection(conn);
-        }catch(Exception e) {
-            System.out.println("nu merge====================Adaugarea");
-            e.printStackTrace();
-        }
+        DB.bagaPriza(Nume, tip, Descriere, Latitude, Longitude);
     }
     @RequestMapping("/update")
     public void updatePriza (@RequestParam(value = "nume") String Nume,
                              @RequestParam(value = "tip") int tip,
                              @RequestParam(value = "descriere") String Descriere,
                              @RequestParam(value  = "id") int id){
+        DB.updatePriza(Nume, tip, Descriere, id);
 
-        try{
-            Connection conn = connectionPool.getConnection();
-            Statement stmt = null;
-            conn.setAutoCommit(false);
-            stmt = conn.createStatement();
-            stmt.executeUpdate("UPDATE \"priza\".\"detalii\" SET nume = "+Nume+", tip = "+tip+", descriere= "+Descriere+"WHERE id="+id+";");
-            stmt.close();
-            conn.commit();
-            connectionPool.releaseConnection(conn);
-        }catch(Exception e) {
-            System.out.println("nu merge===================UPdate");
-            e.printStackTrace();
-        }
     }
-
-
-
-
     @RequestMapping("/getBackOfficeData")
     public List<Priza> getBackOfficeData()
     {
-        List<Priza> rez = new ArrayList<Priza>();
-        try{
-            Connection conn = connectionPool.getConnection();
-            Statement stmt = null;
-
-            conn.setAutoCommit(false);
-            System.out.println("Opened database successfully");
-            stmt = conn.createStatement();
-            ResultSet rs= stmt.executeQuery("select * FROM \"priza\".\"detalii\";");
-            while(rs.next()) {
-                Priza to = new Priza();
-                to.setId(rs.getInt("id"));
-                to.setName(rs.getString("nume"));
-                to.setTip(rs.getInt("tip"));
-                to.setDescriere(rs.getString("descriere"));
-                to.setLat(rs.getDouble("lat"));
-                to.setLng(rs.getDouble("lng"));
-                to.setPending(rs.getInt("status"));
-                to.setReports(rs.getInt("reports"));
-                rez.add(to);
-            }
-            stmt.close();
-            connectionPool.releaseConnection(conn);
-        }catch(Exception e) {
-            System.out.println("=================BackOffice=============");
-            e.printStackTrace();
-        }
-        return rez;
-
+        return DB.aduBackOfficeData();
     }
     @RequestMapping("/changeStatus")
     public void changeStatus (@RequestParam(value = "id") int id,
                               @RequestParam(value = "status") int status)
     {
-        try{
-            Connection conn = connectionPool.getConnection();
-            Statement stmt = null;
-            conn.setAutoCommit(false);
-            stmt = conn.createStatement();
-            stmt.executeUpdate("UPDATE \"priza\".\"detalii\" set status = "+status+"WHERE id="+id+";");
-            stmt.close();
-            conn.commit();
-            connectionPool.releaseConnection(conn);
-        }catch(Exception e) {
-            System.out.println("*=======*");
-            e.printStackTrace();
-        }
+        DB.schimbaStatus(id, status);
     }
     @RequestMapping("/report")
     public void report (@RequestParam(value = "id") int id)
     {
-        try {
-            Connection conn = connectionPool.getConnection();
-            Connection conn2= connectionPool.getConnection();
-            Statement stmt = null;
-            Statement stmt2 = null;
-            conn.setAutoCommit(false);
-            conn2.setAutoCommit(false);
-            stmt2 = conn2.createStatement();
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT reports FROM \"priza\".\"detalii\" WHERE id = "+id+";");
-            int tot=1;
-            while(rs.next())
-                tot=rs.getInt("reports");
-            stmt.executeUpdate("UPDATE \"priza\".\"detalii\" set reports = "+(tot+1)+" WHERE id="+id+";");
-            conn.commit();
-            connectionPool.releaseConnection(conn);
-            connectionPool.releaseConnection(conn2);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        DB.raporteaza(id);
     }
-
 }
 
